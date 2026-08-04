eefb9d53fa5aedffd2f8d56afffe4ab2220502cd
oqin <oqin.official@gmail.com>
2026-08-04 23:58:43 +0700

    logo

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -5,6 +5,11 @@
 <title><?php echo isset($title) ? $title : 'Login'; ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
+<link rel="icon" type="image/x-icon" href="<?php echo base_url('assets/favicon/favicon.ico'); ?>">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url('assets/favicon/favicon-32x32.png'); ?>">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('assets/favicon/favicon-16x16.png'); ?>">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('assets/favicon/apple-touch-icon.png'); ?>">
+<link rel="manifest" href="<?php echo base_url('assets/favicon/site.webmanifest'); ?>">
 </head>
 <body>
 <div class="login-wrapper">
